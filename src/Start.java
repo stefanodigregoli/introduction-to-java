f1a1d1643304de4d8d434f9526aab187fb1f1984
ariosto22 <emanuele.digregoli40@gmail.com>
2023-06-11 15:44:54 +0200

    Introduction To Java Create a package called co.develhope.introduction Create class Start inside package co.develhope.introduction Create method main inside class Start Print using System.out.println the string Hello develhope java student and your birthday

---
+++ src/Start.java
@@ -0,0 +1,6 @@
+public class Start {
+    public static void main(String[] args) {
+        String birthday = "March 2";
+        System.out.println("Hello develhope java student " + birthday);
+    }
+}
